b8b04f622c5c1e944fb555fb987dc324ee0a7277
quytm <quytm@vnpay.vn>
2019-12-02 14:27:17 +0700

    add project

---
+++ src/main/java/com/teko/quytm/testinglib/MyLib.java
@@ -0,0 +1,17 @@
+package com.teko.quytm.testinglib;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * Author : Tran Minh Quy
+ * Email: quy.tm@teko.vn
+ * Dec 02, 2019
+ */
+@NoArgsConstructor
+public class MyLib {
+
+    public static String hello(String name) {
+        return "Hello " + name;
+    }
+
+}
---
+++ src/test/java/com/teko/quytm/testinglib/MyLibTest.java
@@ -0,0 +1,14 @@
+package com.teko.quytm.testinglib;
+
+/**
+ * Author : Tran Minh Quy
+ * Email: quy.tm@teko.vn
+ * Dec 02, 2019
+ */
+public class MyLibTest {
+
+    public static void main(String[] args) {
+        System.out.println(MyLib.hello("Quy"));
+    }
+
+}
